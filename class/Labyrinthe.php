7b902c453c21e385d009c3d73eab910a808e385a
MathisD49 <mathisdubuc@gmail.com>
2021-05-04 16:42:21 +0200

    ajout d'un fonction pour le parsing

--- class/Labyrinthe.php
+++ class/Labyrinthe.php
@@ -3,30 +3,36 @@
 
     // attributs
     public $path_txt_file;
-    public $array_game = [];
+    public $test;
 
     // contructeur
     function __construct($path_txt_file){
       $this->path_txt_file = $path_txt_file;
     }
 
-    //methode permetant d'ouvrir, de lire, et d'afficher le contenu de mon fichier texte
-    public function showContent(){
-      $test = file("" . $this->path_txt_file . "");
+    // fonction pour faire de notre jeu un tableau multidim
+    public function parsingGame(){
+      $this->test = file("" . $this->path_txt_file . "");
 
-      foreach ($test as $key => $value) {
+      foreach ($this->test as $key => $value) {
         //echo($tests . "<br>"); // value string
-        $test[$key] = explode(" ", $value);
+        $this->test[$key] = explode(" ", $value);
       }
 
-      foreach ($test as $key => $value) {
-        foreach ($test[$key] as $keys => $values) {
+      return $this->test;
+    }
+
+    //methode permetant d'ouvrir, de lire, et d'afficher le contenu de mon fichier texte
+    public function showContent(){
+      $arrayGame = $this->parsingGame();
+      foreach ($arrayGame as $key => $value) {
+        foreach ($arrayGame[$key] as $keys => $values) {
           echo($values . " ");
         }
         echo("<br>");
       }
 
-      //var_dump($test);
+      //var_dump($this->test);
 
       //$myFileOpen = fopen("" . $this->path_txt_file . "", 'r+');
 
