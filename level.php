ce53cfbdd662374c22385b3bb12965fad6257ff6
MathisD49 <mathisdubuc@gmail.com>
2021-05-18 11:31:45 +0200

    changement dans l'arborescence des fichiers

--- level.php
+++ level.php
@@ -5,11 +5,11 @@
   </head>
   <body>
     <h1>Choisissez votre level <?php echo($_COOKIE["pseudo"]); ?></h1>
-    <a href="game.php?level=labyrinthe.txt">
+    <a href="game.php?level=gamefiles/labyrinthe.txt">
       <span>Level 1</span>
     </a>
     <br>
-    <a href="game.php?level=test.txt">
+    <a href="game.php?level=gamefiles/test.txt">
       <span>Level 2</span>
     </a>
   </body>
